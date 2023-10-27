5cf605e0cfb7644f88275ff21abf4adc9e1a2f70
rani-neo <ishika.rashmi@gmail.com>
2023-10-27 20:43:58 +1100

    change in  comment

--- person.js
+++ person.js
@@ -1,6 +1,12 @@
-const person = {
-   name: 'Jone Doe',
-   age: 30
+class Person {
+   constructor(name, age) {
+      this.name = name;
+      this.age = age;
+   }
+
+   greeting() {
+      console.log(`My name is ${this.name} and I am ${this.age}`);
+   }
 }
 
-module.exports = person;
+module.exports = Person;
